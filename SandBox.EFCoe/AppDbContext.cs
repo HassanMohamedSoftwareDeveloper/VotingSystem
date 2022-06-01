ddf0d50628f4691778e2ce5d51a4f4c0d21f6762
Hassan Mohamed <HassanMohamed_HM@hotmail.com>
2022-06-01 14:34:55 +0200

    EF Sandbox

---
+++ SandBox.EFCoe/AppDbContext.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SandBox.EFCoe;
+
+public class AppDbContext : DbContext
+{
+    public DbSet<Fruit> Fruits { get; set; }
+    public DbSet<Address> Addresses { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Fruit>().Property<int>("Id");
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseInMemoryDatabase("Database");
+    }
+}
+public class Fruit
+{
+    public string Name { get; set; }
+    public int Weight { get; set; }
+    public Address Address { get; set; }
+}
+public class Address
+{
+    public int Id { get; set; }
+    public string PostCode { get; set; }
+}
+public class FruitVm
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public int Weight { get; set; }
+    public string PostCode { get; set; }
+}
---
+++ SandBox.EFCoe/Program.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SandBox.EFCoe;
+
+
+
+using (var ctx = new AppDbContext())
+{
+    var orange = new Fruit { Name = "Orange" };
+    var apple = new Fruit { Name = "Apple" };
+
+    ctx.Fruits.Add(orange);
+    var orangeId = ctx.Entry(orange).Property<int>("Id").CurrentValue;
+    ctx.Fruits.Add(apple);
+
+    var address = new Address { PostCode = "Moon" };
+    //ctx.Addresses.Add(address);
+    orange.Address = address;
+    ctx.SaveChanges(); 
+}
+
+using (var ctx = new AppDbContext())
+{
+    var fruits = ctx.Fruits.Select(x => new FruitVm
+    {
+        Id = EF.Property<int>(x, "Id"),
+        Name = x.Name,
+        Weight = x.Weight,
+        PostCode=x.Address.PostCode
+    }).ToList();
+
+
+    var fullFruits = ctx.Fruits.ToList();
+    var addresses = ctx.Addresses.ToList(); 
+}
+Console.ReadKey();
